91656172f309eba34a12685b71bddced59fe1098
ishland <ishlandmc@yeah.net>
2022-12-26 14:43:02 +0800

    1.19.3

--- src/main/java/com/ishland/fabric/rsls/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/com/ishland/fabric/rsls/mixin/MixinClientPlayNetworkHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.Packet;
 import net.minecraft.network.listener.PacketListener;
 import net.minecraft.util.thread.ThreadExecutor;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -11,7 +12,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ClientPlayNetworkHandler.class)
 public class MixinClientPlayNetworkHandler {
 
-    @Redirect(method = {"onPlaySound", "onPlaySoundId"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V"), require = 2)
+    @Dynamic
+    @Redirect(method = {"onPlaySound", "method_11104"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;Lnet/minecraft/util/thread/ThreadExecutor;)V"), require = 1)
     private <T extends PacketListener> void handleSoundsAsync(Packet<T> packet, T listener, ThreadExecutor<?> engine) {
         // no-op as we don't want this to be executed sync
     }
